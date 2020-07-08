1824150bdb6842102463ddc5d66f77042c90a9d4
Miguel Lopez <malopezp.91@gmail.com>
2020-07-08 00:09:57 -0500

    Implementation of Adding functionality

--- src/main/java/com/captec/challenge/calculator/Calculator.java
+++ src/main/java/com/captec/challenge/calculator/Calculator.java
@@ -1,7 +1,62 @@
 package com.captec.challenge.calculator;
 
+import static com.captec.challenge.calculator.Utils.ADD_CHAR;
+import static com.captec.challenge.calculator.Utils.DIGITS;
+import static com.captec.challenge.calculator.Utils.ERROR;
+import static com.captec.challenge.calculator.Utils.VALID_SYMBOLS;
+
 /**
- *
+ * Calculator
+ * Contains all logic for adding, clearing and equals.
  */
 public class Calculator {
+
+    private String display;
+    private int previousValue = 0;
+    private int currentValue = 0;
+
+    public String process(char input) {
+        if (VALID_SYMBOLS.contains(input)) {
+            processSymbol(input);
+            return display;
+        } else if (DIGITS.contains(input)) {
+            processDigit(input);
+            return display;
+        }
+        return null;
+    }
+
+    private void processSymbol(char symbol) {
+        if (ADD_CHAR == symbol) {
+            processAdd();
+        }
+    }
+
+    private void processAdd() {
+        try {
+            int result = Math.addExact(previousValue, currentValue);
+            previousValue = result;
+            currentValue = 0;
+            display = String.valueOf(result);
+        } catch (ArithmeticException e) {
+            reset();
+            display = ERROR;
+        }
+    }
+
+    private void processDigit(char digit) {
+        try {
+            int tempValue = Math.multiplyExact(currentValue, 10);
+            currentValue = Math.addExact(tempValue, Integer.parseInt(Character.toString(digit)));
+            display = String.valueOf(currentValue);
+        } catch (ArithmeticException e) {
+            reset();
+            display = ERROR;
+        }
+    }
+
+    private void reset() {
+        previousValue = 0;
+        currentValue = 0;
+    }
 }
